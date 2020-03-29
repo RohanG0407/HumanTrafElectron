4b5b4166adcbc0c98756e703a6f4731c35ee6940
andrewsomeister <48100886+andrewsomeister@users.noreply.github.com>
2020-03-29 02:28:47 -0400

    Add files via upload

--- input.js
+++ input.js
@@ -1,36 +1,35 @@
-const realFileBtn = document.getElementById("real-file");
-const realReqBtn = document.getElementById("real-requestbutton");
-const customBtn = document.getElementById("custom-button");
-const customTxt = document.getElementById("custom-text");
-const customRequestBtn = document.getElementById("custom-requestbutton");
+var realFileBtn = document.getElementById("real-file");
+var realReqBtn = document.getElementById("submitRequests");
+var customBtn = document.getElementById("custom-button");
+var customTxt = document.getElementById("custom-text");
+var customRequestBtn = document.getElementById("custom-requestbutton");
+var requestTxt = document.getElementById("request-text");
 var fs = require('fs');
 var path = require('path');
 customBtn.addEventListener("click", function () {
     realFileBtn.click();
 });
 
-//customRequestBtn.addEventListener("click", function () {
-//    realReqBtn.click();
-//});
-//realReqBtn.addEventListener("change", function () {
-//     let requestInfo = new String[4];
-//   if (realReqBtn.value) {
-//        requestInfo[0] = document.getElementById("carColor").value;
-//        requestInfo[1] = document.getElementById("carModel").value;
-//        requestInfo[2] = document.getElementById("carAdress").value;
-//        requestInfo[3] = document.getElementById("carRadius").value;
-//        document.setElementById("carColor").innerHTML("");
-//        document.setElementById("carModel").innerHTML = "";
-//        document.setElementById("carAdress").innerHTML = "";
-//        document.setElementById("carRadius").innerHTML = "";
-//    }
-//});
+realReqBtn.addEventListener("click", function(){
+    document.getElementById("car_color").value = "";
+    document.getElementById("car_model").value = "";
+    document.getElementById("address").value = "";
+    document.getElementById("radius").value = "";
+    
+    requestTxt.innerHTML = "Request Sending...";
+    
+        setTimeout(function(){
+          requestTxt.innerHTML = "Request Sent";
+            }, 3000);
+
+});
 
 realFileBtn.addEventListener("change", function () {
     if (realFileBtn.value) {
         customTxt.innerHTML = realFileBtn.value.match(
             /[\/\\]([\w\d\s\.\-\(\)]+)$/
-        )[1];
+        )[1]
+        customTxt.innerHTML += " uploaded successfully!";
 
         const thePath = path.join(document.getElementById('real-file').files[0].path);
         const newPath = path.join(__dirname, '/uploads/test.mp4');
